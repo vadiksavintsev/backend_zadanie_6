fa5b410a92da9a2ad4904f9be320a75c4cca9262
vadiksavintsev <113433935+vadiksavintsev@users.noreply.github.com>
2023-05-31 12:23:10 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -156,7 +156,7 @@ transform: scale(1.5);
             
                 <td>
                     <?php foreach ($power as $power){
-                            <input type="checkbox" name="power[<?= $user_id ?>][]" value="<?= $power['id'] ?>" <?= in_array($power['id'], $user_abilities) ? 'checked' : '' ?>>
+                            <input type="checkbox" name="power[<?= $user_id ?>][]" value="<?= $power['id'] ?>" <?= in_array($power['id'], $user_abilities) ? 'checked' : ''>
                             <?= htmlspecialchars($power['power']) ?>
         }
                      endforeach; ?>
